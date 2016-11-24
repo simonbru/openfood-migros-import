0fb35cb0bcccd206b83311ff4d0e4d409029ff77
Simon Brulhart <simon@brulhart.me>
2016-11-24 18:27:23 +0100

    WIP

--- filter_existing_products.py
+++ filter_existing_products.py
@@ -4,7 +4,10 @@
 Requires psycopg2 library to access the postgres database
 """
 
+import csv
+import itertools
 import json
+import sys
 from pathlib import Path
 
 import psycopg2
@@ -39,17 +42,38 @@ def product_exists_in_openfood(cursor, product):
     return cursor.rowcount == 1
 
 
+def csv_output(output_file, cur, products):
+    writer = csv.writer(output_file)
+    writer.writerow(['name', 'state', 'migros_id', 'barcode in OpenFood', 'other barcodes'])
+    for product_id, product in itertools.islice(products, 100):
+        try:
+            if not product_exists_in_openfood(cur, product):
+                writer.writerow((
+                    product['title'], 'present', product_id, ' ', ','.join(product['_source']['eans'])
+                ))
+            else:
+                writer.writerow((
+                    product['title'], 'missing', product_id, ' ', ','.join(product['_source']['eans'])
+                ))
+        except InvalidProductError as e:
+            print("Error parsing migros product {}: {}".format(product_id, e))
+
+
 def main():
     conn = psycopg2.connect(database=DATABASE)
     cur = conn.cursor()
-    for product_id, product in migros_products():
-        try:
-            if not product_exists_in_openfood(cur, product):
-                print("missing: " + product_id)
-            else:
-                print("ok: " + product_id)
-        except InvalidProductError as e:
-            print("Error parsing migros product {}: {}".format(product_id, e))
+    if len(sys.argv) == 3 and sys.argv[1] == '--csv':
+        with open(sys.argv[2], 'w') as f:
+            csv_output(f, cur, migros_products())
+    else:
+        for product_id, product in migros_products():
+            try:
+                if not product_exists_in_openfood(cur, product):
+                    print("missing: " + product_id)
+                else:
+                    print("ok: " + product_id)
+            except InvalidProductError as e:
+                print("Error parsing migros product {}: {}".format(product_id, e))
 
 
 if __name__ == '__main__':
